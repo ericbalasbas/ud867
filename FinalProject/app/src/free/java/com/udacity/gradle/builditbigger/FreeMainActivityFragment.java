fff5a0cf64b18f5f1f615a8e16f599fd6fc4a4cd
Eric Balasbas <ericbalasbas@users.noreply.github.com>
2017-06-23 13:28:38 -0400

    Add interstitial ad

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/FreeMainActivityFragment.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/FreeMainActivityFragment.java
@@ -1,14 +1,19 @@
 package com.udacity.gradle.builditbigger;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
@@ -19,10 +24,12 @@ public class FreeMainActivityFragment extends Fragment {
     public FreeMainActivityFragment() {
     }
 
+    private InterstitialAd mInterstitialAd;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.free_fragment_main, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
@@ -32,6 +39,36 @@ public class FreeMainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        final Button button = (Button) root.findViewById(R.id.joke_button_id);
+
+        // Show interstitial ad before joke
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
+            }
+        });
+
+       // Show joke when ad is closed
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                new EndpointsAsyncTask().execute(getContext());
+            }
+
+        });
+
         return root;
     }
 }
+
+
